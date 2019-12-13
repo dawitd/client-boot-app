fc50bb93ee99b4e539d509e9143629e6b71462b9
dfantaw <dfantaw@gmail.com>
2019-12-13 15:08:20 -0600

    add facebook login

--- src/main/java/com/sn/app/Controllers/HomeController.java
+++ src/main/java/com/sn/app/Controllers/HomeController.java
@@ -1,11 +1,21 @@
 package com.sn.app.Controllers;
 
 import org.springframework.http.*;
+import org.springframework.social.connect.Connection;
+import org.springframework.social.facebook.api.Facebook;
+import org.springframework.social.facebook.api.User;
+import org.springframework.social.facebook.connect.FacebookConnectionFactory;
+import org.springframework.social.oauth2.AccessGrant;
+import org.springframework.social.oauth2.OAuth2Operations;
+import org.springframework.social.oauth2.OAuth2Parameters;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.client.RestTemplate;
+import org.springframework.web.servlet.ModelAndView;
 
 import java.util.Arrays;
 
@@ -79,4 +89,39 @@ public class HomeController {
         return "editprofile";
     }
 
+    private FacebookConnectionFactory factory = new FacebookConnectionFactory("459570614997343",
+            "527354cb4319414c73a7835ec7975ad6");
+
+
+
+    @GetMapping(value = "/fb_login")
+    public String fb_login() {
+
+        OAuth2Operations operations = factory.getOAuthOperations();
+        OAuth2Parameters params = new OAuth2Parameters();
+
+        params.setRedirectUri("http://localhost:8090/forwardLogin");
+        params.setScope("email,public_profile");
+
+        String url = operations.buildAuthenticateUrl(params);
+        System.out.println("The URL is" + url);
+        return "redirect:" + url;
+
+    }
+
+    @RequestMapping(value = "/forwardLogin")
+    public ModelAndView prodducer(@RequestParam("code") String authorizationCode) {
+        OAuth2Operations operations = factory.getOAuthOperations();
+        AccessGrant accessToken = operations.exchangeForAccess(authorizationCode, "http://localhost:8090/forwardLogin",
+                null);
+
+        Connection<Facebook> connection = factory.createConnection(accessToken);
+        Facebook facebook = connection.getApi();
+        String[] fields = {"id", "email", "first_name", "last_name"};
+        User userProfile = facebook.fetchObject("me", User.class, fields);
+        ModelAndView model = new ModelAndView("newsfeed");
+        model.addObject("user", userProfile);
+        return model;
+
+    }
 }
